6a2abe6b11ded1dc311933daf3e233c0e4f0d813
saiprathikgaddamidi-max <2438952@cognizant.com>
2025-09-18 17:43:56 +0530

    DbContextet Added

---
+++ FleetManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FleetManagementSystem.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        
+
+    }
+}
